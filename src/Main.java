1609f8f374f23e8ba2126f7834502d3a55fa2587
pete8550 <42993922+pete8550@users.noreply.github.com>
2018-09-26 20:01:37 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("og");
+        System.out.println("Hello Datamatikere!");
+        System.out.println("Held og lykke");
+        System.out.println("Med det hele.");
+    }
+}
